45d5e4dcc23526d18fd7a7ebd31a4f16238962c5
Yash Goyal <yashgo0018@gmail.com>
2023-04-02 22:03:50 +0530

    added openai for meme generation

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI, UploadFile, HTTPException
-from .classifier import load_model, classify
+from . import config
+
+from .meme_generator import combine_image_audio, generate_text_meme, combine_image_text
+from .classifier import classify, load_model
 from PIL import Image
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, HTTPException, UploadFile, Body
 import numpy as np
 import io
 
+
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 model = load_model()
 
 
@@ -14,11 +21,15 @@ def index():
 
 
 @app.post("/generate-meme")
-async def generate_meme(file: UploadFile):
+async def generate_meme(file: UploadFile, topic: str = Body()):
     if "image" not in file.content_type:
         raise HTTPException(400, detail="Invalid document type")
     request_object_content = await file.read()
-    img = Image.open(io.BytesIO(request_object_content))
-    img = np.array(img.resize((256, 256)).convert('RGB'))
+    img_org = Image.open(io.BytesIO(request_object_content))
+    img = np.array(img_org.resize((256, 256)).convert('RGB'))
     predictions = classify(model, img)
-    return {"prediction": predictions}
+    emotion = predictions[0][0]
+    meme = generate_text_meme(emotion, topic)
+    meme_img = combine_image_text(img, meme)
+    video_file_name = combine_image_audio(meme_img, emotion)
+    return {"meme": meme, "video": f"/static/{video_file_name}"}
--- backend/src/meme_generator.py
+++ backend/src/meme_generator.py
@@ -1,6 +1,35 @@
+from moviepy.editor import AudioFileClip, ImageClip
+from random import randint
+import openai
+import os
+
+openai.organization = os.getenv("OPENAI_ORG_ID")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+def generate_text_meme(emotion: str, topic: str):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"give me a {emotion} meme about '{topic}', give only text in less than 20 words\n",
+        temperature=0.7,
+        max_tokens=256,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    print(response)
+    return response["choices"][0]["text"].strip()
+
+
 def combine_image_text(image, text):
-    pass
+    return image
 
 
 def combine_image_audio(image, emotion):
-    pass
+    audio = AudioFileClip("song.mp3")
+    clip = ImageClip(image).set_duration(audio.duration)
+    clip = clip.set_audio(audio)
+    filename = f"{str(randint(1e7, 1e7-1))}.mp4"
+    output_path = f"./static/{filename}"
+    clip.write_videofile(output_path, fps=24)
+    return filename
